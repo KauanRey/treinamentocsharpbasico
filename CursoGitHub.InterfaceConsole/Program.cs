a4e88a304f3f34e1656003ef484726150ff2d621
KauanRey <kauanreycon934@gmail.com>
2022-07-04 11:59:32 -0300

    testando

--- CursoGitHub.InterfaceConsole/Program.cs
+++ CursoGitHub.InterfaceConsole/Program.cs
@@ -1,17 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CursoGitHub.InterfaceConsole
-{
+{ //inicio do namespace
     internal class Program
-    {
+    { //inicio da classe
         static void Main(string[] args)
-        {
+        { //inicio do método
+            /*
+             * Main - principal
+             * O método inicia-se com a letra maíscula
+             *  
+             */
+            //Variável
+            /*
+             * É um espaço reservado na memória
+             */
+            string pergunta;
+            string resposta = "git init";
+            Console.WriteLine("Qual comando para iniciar o git");
+            pergunta = Console.ReadLine();
+            if (pergunta.Equals(resposta))
+            {
+                Console.WriteLine("acertou");
+            }
+            else
+            {
+                Console.WriteLine("errou");
+            }
+            Console.WriteLine($"A resposta é {resposta}");
             Console.WriteLine("Alô Mundo!");
             Console.ReadKey();
-        }
-    }
-}
+
+        } //fim do método
+    } //fim da classe
+} //fim do namespace
+
